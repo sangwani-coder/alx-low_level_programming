a5d49cf1771c69a770419d82df1042d482080aaa
zyambo <mightypz2@gmail.com>
2021-08-27 01:46:43 +0200

    Task6-sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 	char b[7] = "byte(s)";
+
 	printf("Size of a char: %d %s\n", sizeof(char), b);
-	printf("Size of an int: %d %s\n", sizeof(int), "byte(s)");
-	printf("Size of a long int: %d %s\n", sizeof(long int), "byte(s)");
-	printf("Size of a long long int: %d %s\n", sizeof(long long int), "byte(s)");
-	printf("Size of a float: %d %s\n", sizeof(float), "byte(s)");	
+	printf("Size of an int: %d %s\n", sizeof(int), b);
+	printf("Size of a long int: %d %s\n", sizeof(long int), b);
+	printf("Size of a long long int: %d %s\n", sizeof(long long int), b);
+	printf("Size of a float: %d %s\n", sizeof(float), b);
 	return (0);
-
-
 }
