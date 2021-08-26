c73075210eb7c38e02a83ebd4fad36a0b646e6eb
zyambo <mightypz2@gmail.com>
2021-08-27 00:43:52 +0200

    Task 4 Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/*
- * main Entry point
- * 
- * Return Always 0 (success)
+/**
+ * main - Entry point to the program
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
 }
