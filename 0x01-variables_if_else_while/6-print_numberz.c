b5a068812b3cf8bdf1c7eae0b2ab448a90249fe5
zyambo <mightypz2@gmail.com>
2021-08-28 00:59:31 +0200

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int num;
+	/* for loop to get alphabet character*/
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num%10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
