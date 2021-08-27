97e2ad76f984411f70c7945dc6cf5b44863a95a3
zyambo <mightypz2@gmail.com>
2021-08-27 22:30:50 +0200

    Task8-stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  * Return: Return 1
  */
 
-int main()
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,
+		2015-10-19\n");
 	return (1);
 }
