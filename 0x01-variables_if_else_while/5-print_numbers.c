1d6be0d6fc7d0e1dda6f94220624c84ec8251489
zyambo <mightypz2@gmail.com>
2021-08-28 00:43:39 +0200

    Task5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Success (0)
+ */
+
+int main(void)
+{
+	int num;
+	/* for loop to get alphabet character*/
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
