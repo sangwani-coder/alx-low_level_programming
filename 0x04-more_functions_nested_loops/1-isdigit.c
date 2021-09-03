e3cad63e406aaf399cbeed4240f1ea331128bd59
zyambo <mightypz2@gmail.com>
2021-09-03 21:26:35 +0200

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isdigit - chech if a input is a digit
+ *@c: parameter
+ *
+ * Return: Always 1 if param is a digit and 0 otherwise
+ */
+
+
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
