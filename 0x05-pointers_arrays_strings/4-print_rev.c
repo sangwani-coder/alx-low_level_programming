5e2b6084c3af04a18074814c573390a2f4c33991
zyambo <mightypz2@gmail.com>
2021-09-07 13:49:45 +0200

    fifth commit reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* print_rev - print out a string in reverse
+* @s: string variable
+*
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
