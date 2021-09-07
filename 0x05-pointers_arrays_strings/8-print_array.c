5234dc9a6653456542fbf27188dc71f561c21cae
zyambo <mightypz2@gmail.com>
2021-09-07 17:36:52 +0200

    nineth commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array- print array
+ *@a: array
+ *@n: array size
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[i]);
+}
