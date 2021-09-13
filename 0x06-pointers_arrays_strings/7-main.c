38d233b8efb5d94e4c6b9c6ccaaf3b6f8e1dc202
zyambo <mightypz2@gmail.com>
2021-09-13 22:36:31 +0200

    final commit

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main -check code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes. \n;
+	char *p;
+
+	p = leet();
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
+
+
