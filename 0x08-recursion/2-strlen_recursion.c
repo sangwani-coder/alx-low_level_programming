da22e70263b709c444e4e472f8381f8a6b99e532
zyambo <mightypz2@gmail.com>
2021-09-16 05:55:27 +0200

    fix betty style bugs

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,17 +4,20 @@
  *_strlen_recursion - print the length of a string cursively
  *@s: string whose len is to  be calculated
  *
- *Return length of s
+ *Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
 	int count;
+
 	if (*s == '\0')
+	{
 		return (0);
+	}
 	else
 	{
-		count =	1 +_strlen_recursion(s + 1);
+		count =	1 + _strlen_recursion(s + 1);
 	}
 	return (count);
 
