16504c8658f0fdc2b63fff20d3f5d80f52049b52
zyambo <mightypz2@gmail.com>
2021-09-23 19:01:02 +0200

    task number one malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * malloc_checked - allocated memory using malloc
+ * @b: int param
+ *
+ * Return: pointer to allocated memory
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr; 
+
+       	ptr = (int*) malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	printf("allocated\n");
+	return (ptr);
+}
