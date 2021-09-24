40f813ba87badb8199b478170bf720951f728946
zyambo <mightypz2@gmail.com>
2021-09-24 06:31:55 +0200

    check if NULL is passed as argument

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 /**
  * string_nconcat - Concatenate two strings
  * @s1: string 1
@@ -11,14 +10,11 @@
  *
  * Return: pointer to newlly allocated space in memory
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-	{
 		if (s1 != NULL && s2 == NULL)
 		{
 			s2 = " ";
@@ -26,41 +22,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		else if (s1 == NULL && s2 != NULL)
 		{
 			s1 = " ";
-			ptr = (char *) malloc(strlen(s1) * n * sizeof(char));
+
 		}
 		else if (s1 == NULL && s2 == NULL)
 		{
 			s1 = " ";
 			s2 = " ";
 		}
-		/*Insert first string in the new string*/
-		while (s1[i] != '\0')
-		{
-			ptr[j] = s1[i];
-			i++;	
-			j++;
-			
-		}
-		/*Insert second string into new string*/
-		i = 0;
-		while (i != n || i != '\0')
-		{
-			ptr[j] = s2[i];
-			i++;
-			j++;
-		}
-		ptr[j] = '\0';
-
-		return (ptr);
-	}
 	ptr = (char *) malloc(strlen(s1) * n * sizeof(char));
 	/*Insert first string in the new string*/
 		while (s1[i] != '\0')
 		{
 			ptr[j] = s1[i];
-			i++;	
+			i++;
 			j++;
-			
+
 		}
 		/*Insert second string into new string*/
 		i = 0;
