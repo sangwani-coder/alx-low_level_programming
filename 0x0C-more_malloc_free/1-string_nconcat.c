7194219e83bd47c25785d36ba05f8bc83284e461
zyambo <mightypz2@gmail.com>
2021-09-24 06:21:21 +0200

    check if NULL is passed as argument

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,24 +17,60 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0;
 	char *ptr;
 
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s1 != NULL && s2 == NULL)
+		{
+			s2 = " ";
+		}
+		else if (s1 == NULL && s2 != NULL)
+		{
+			s1 = " ";
+			ptr = (char *) malloc(strlen(s1) * n * sizeof(char));
+		}
+		else if (s1 == NULL && s2 == NULL)
+		{
+			s1 = " ";
+			s2 = " ";
+		}
+		/*Insert first string in the new string*/
+		while (s1[i] != '\0')
+		{
+			ptr[j] = s1[i];
+			i++;	
+			j++;
+			
+		}
+		/*Insert second string into new string*/
+		i = 0;
+		while (i != n || i != '\0')
+		{
+			ptr[j] = s2[i];
+			i++;
+			j++;
+		}
+		ptr[j] = '\0';
+
+		return (ptr);
+	}
 	ptr = (char *) malloc(strlen(s1) * n * sizeof(char));
-
 	/*Insert first string in the new string*/
-	while (s1[i] != '\0')
-	{
-		ptr[j] = s1[i];
-		i++;
-		j++;
-	}
-	/*Insert second string into new string*/
-	i = 0;
-	while (i != n)
-	{
-		ptr[j] = s2[i];
-		i++;
-		j++;
-	}
-	ptr[j] = '\0';
+		while (s1[i] != '\0')
+		{
+			ptr[j] = s1[i];
+			i++;	
+			j++;
+			
+		}
+		/*Insert second string into new string*/
+		i = 0;
+		while (i != n)
+		{
+			ptr[j] = s2[i];
+			i++;
+			j++;
+		}
+		ptr[j] = '\0';
 
-	return (ptr);
+		return (ptr);
 }
