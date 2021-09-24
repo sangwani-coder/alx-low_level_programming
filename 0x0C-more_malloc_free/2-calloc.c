592c3ec3b204f017e54ba2d0b09d1e842f0fd41a
zyambo <mightypz2@gmail.com>
2021-09-24 22:25:48 +0200

    task 2 calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - allocate memory for an array
@@ -12,15 +13,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	if (nmemb == 0 || size == 0)
+	else
 	{
-		return (NULL);
+		bzero(ptr, nmemb * size);
+		return (ptr);
 	}
-	return (ptr);
+	return (0);
 }
