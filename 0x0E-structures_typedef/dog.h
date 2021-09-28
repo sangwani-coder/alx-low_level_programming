0784845150b1305849076e3847dc8b7ec1376228
zyambo <mightypz2@gmail.com>
2021-09-28 09:45:47 +0200

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new dog struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+
+
+struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif
