697aa3613d98e44b05a1f0969b144f3e2228e33e
zyambo <mightypz2@gmail.com>
2021-09-28 09:58:09 +0200

    secodn task commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initalize a variable of type struct dog
+ * @d: dog struct
+ * @name: dog name char
+ * @age: dog age int
+ * @owner: dog owner char
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
