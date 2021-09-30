0aac649e50ac1b727a1f0ebd9f74275337c2d01e
zyambo <mightypz2@gmail.com>
2021-09-30 07:58:11 +0200

    fix bugs

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
-#ifndef _FUNCTION_POINNTERS_H
-#define _FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINNTERS_H
+#define FUNCTION_POINTERS_H
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
