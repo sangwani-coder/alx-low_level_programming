0f7e994f6be9b84438ed8c3e12ffbde4006ab63f
zyambo <mightypz2@gmail.com>
2021-10-12 23:45:00 +0200

    fix bugs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,20 +15,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	ptr->n = n; /*Creating a new node*/
 	temp = *head;
-	
-	if ( idx == 1)
+
+	if (idx == 1)
 	{
 		ptr->next = temp;
 		*head = ptr;
 		return (0);
 	}
-	
-	for ( i = 1; i < idx - 1; i++)  /*moving to the (n-1)th position node in the linked list*/
+
+	for (i = 1; i < idx - 1; i++)
 	{
 		temp = temp->next;
 	}
-	ptr->next = temp->next;  /* the newly created node point to next node of ptr temp*/
-	temp->next = ptr;  	/*Make ptr temp point to newly created node in the linked list*/
+	ptr->next = temp->next;
+	temp->next = ptr;
 	return (0);
 }
-
