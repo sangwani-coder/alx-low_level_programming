80592bb4f3a1a6c535a725ce450b5ba9a33022bc
zyambo <mightypz2@gmail.com>
2021-10-12 10:04:48 +0200

    fix error in return value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->next = NULL;
 	temp = *head;
 
+	if (newNode == NULL)
+		return (NULL);
 	if (idx == 1)
 	{
 		newNode->next = temp;
