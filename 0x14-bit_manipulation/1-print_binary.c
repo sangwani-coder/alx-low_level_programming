9cfa3008c8661985234ddf0499de64fed31da6c0
zyambo <mightypz2@gmail.com>
2021-10-15 21:13:17 +0200

    fix bug in if statement

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,9 +10,7 @@ void print_binary(unsigned long int n)
 	unsigned long int bin;
 
 	if (n > 1)
-	{
-		print_binary(n >> 1);
-		bin = n & 1;
-		_putchar(binary + '0');
-	}
+	print_binary(n >> 1);
+	bin = n & 1;
+	_putchar(bin + '0');
 }
