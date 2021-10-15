59b98078534ad8b0754da1ae0028c5afb993427e
zyambo <mightypz2@gmail.com>
2021-10-15 21:19:35 +0200

    First commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - get the index of a bit
+ * @n: number to get bit from
+ * @index: index of the bit starting at 0
+ *
+ * Return: Value of a bit at given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit_status;
+
+	/*Right shift n, index times and perform bitwise AND with 1*/
+	bit_status = (n >> index) & 1;
+	return (bit_status);
+}
