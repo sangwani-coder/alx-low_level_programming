3a45a0d2affc9a17e0890f0f6546e61f0c85f0af
zyambo <mightypz2@gmail.com>
2021-10-18 23:29:26 +0200

    Fix check fails

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,8 @@
-#include "main.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <elf.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
