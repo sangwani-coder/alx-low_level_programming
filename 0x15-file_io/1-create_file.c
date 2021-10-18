fd168b745b40659f628b01a46a8cb97337c63390
zyambo <mightypz2@gmail.com>
2021-10-18 22:39:48 +0200

    Fix check 7 fail

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-		wrt = write(opn, " ", 0);
+		return (1);
 	}
 	if (text_content != NULL)
 	{
