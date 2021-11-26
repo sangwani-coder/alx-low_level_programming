1e34c3dd96c123e0c65fc04321a0acfdfc8ed4a3
zyambo <mightypz2@gmail.com>
2021-11-26 21:43:06 +0200

    debug 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,74 +12,29 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = NULL, *new_node = NULL;
-	unsigned int iter_times = 0, len = 0;
+	dlistint_t *tmp = *h, *new;
 
-	if (h == NULL)
-		return (NULL);
-
-	if (*h == NULL && idx == 0)
-		return (add_dnodeint(h, n));
-
-	len = dlistint_len(*h);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (len == idx)
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	current = *h;
-	while (current != NULL)
-	{
-		if (iter_times == idx)
-		{
-			new_node = create_node(n, current, current->next);
-			current->prev = new_node;
-			current = new_node;
-			current->prev->next = new_node;
-			return (new_node);
-		}
-		current = current->next;
-		++iter_times;
-	}
-	return (current);
-}
-
-/**
- *dlistint_len - counts the number of elements in doubly linked list
- *@h: The double linked list to count
- *
- * Return: Number of elements in the doubly linked list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	int len = 0;
-
-	while (h != NULL)
-	{
-		++len;
-		h = h->next;
-	}
-	return (len);
-}
-
-/**
- *create_node - Create a new node with value
- *@n: The number of the new node
- *@next: The next node of the new node
- *@prev: The previous node of new node
- *
- * Return: The address of new node created
- */
-dlistint_t *create_node(unsigned int n, void *next, void *prev)
-{
-	dlistint_t *new_node = NULL;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = next;
-	new_node->prev = prev;
-	return (new_node);
+	new->n = n;
+	new->next = tmp;
+	tmp->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
