3b15b75da3dd841a175130ca27901ffe4342bc97
sangwani <mightypz2@gmail.com>
2021-12-17 23:07:37 +0200

    add 100

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,4 +45,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
+/*ADVANCED SHASH FUNCTIONS*/
+shash_table_t *shash_table_create(unsigned long int size);
+
 #endif /* HASH_TABLES */
