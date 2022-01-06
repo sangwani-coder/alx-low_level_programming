733a6f9bb6e1c930860b419dc893c75ac7b14ff2
sangwani-coder <mightypz2@gmail.com>
2022-01-06 11:06:43 +0200

    debug

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -48,5 +48,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(char *s);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2);
 #endif
